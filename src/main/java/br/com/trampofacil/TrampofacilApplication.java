e6dfdd22af3cfc89f44c829ae7b229408f75f360
erick <erickvinicius@erick-mint>
2018-05-12 17:33:59 -0300

    Primeiro commit criação do projeto

---
+++ src/main/java/br/com/trampofacil/TrampofacilApplication.java
@@ -0,0 +1,12 @@
+package br.com.trampofacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrampofacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrampofacilApplication.class, args);
+	}
+}
